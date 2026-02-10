83cb1ed6bb2429d075f9be73e890725ac17ce3d4
DamjanIgnjatic <ignjatic.dev@gmail.com>
2026-02-10 16:15:25 +0100

    code change

--- wp-content/themes/stella-horeca/blocks/contact-section/contact-section.php
+++ wp-content/themes/stella-horeca/blocks/contact-section/contact-section.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /* 
 * Block Name: Image
 * Post Type: page, post
@@ -8,38 +8,22 @@
     <?php do_action('block_image', __FILE__); ?>
 <?php else: ?>
     <?php
-        // Include helpers
-        include get_template_directory() . '/template-parts/base/block-helper.php';
+    // Include helpers
+    include get_template_directory() . '/template-parts/base/block-helper.php';
 
-        // Load values and assing defaults
-        $title = get_field('title') ?: false;
+    // Load values and assing defaults
+    $title = get_field('title') ?: false;
     ?>
-    <section class="section contact-section-section-wrapper block <?php echo $className; ?>" 
+    <section class="section section-contact block <?php echo $className; ?>"
         <?php if ($bgUrl): ?>
-            style="background-image: url('<?php echo $bgUrl; ?>');"
-            data-desktop-image="<?php echo $bgUrl; ?>"
+        style="background-image: url('<?php echo $bgUrl; ?>');"
+        data-desktop-image="<?php echo $bgUrl; ?>"
         <?php endif; ?>
         <?php if ($mobileBgUrl): ?>
-            data-mobile-image="<?php echo $mobileBgUrl; ?>"
-        <?php endif; ?>
-        <?php if ($sectionId): ?>
-            id="<?php echo $sectionId; ?>"
-        <?php endif; ?>
-    >
+        data-mobile-image="<?php echo $mobileBgUrl; ?>"
+        <?php endif; ?>>
         <div class="container">
-            <?php if ($contentWidth): ?>
-                <div class="row">
-                    <div class="<?php echo $contentWidth; ?>">
-            <?php endif; ?>
-                <!-- Content START -->
-                <?php if ($title): ?>
-                    <h2><?php echo $title; ?></h2>
-                <?php endif; ?>
-                <!-- Content END -->
-            <?php if ($contentWidth): ?>
-                    </div>
-                </div>
-            <?php endif; ?>
+
         </div>
     </section>
 <?php endif; ?>
