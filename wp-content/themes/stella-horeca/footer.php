d78245c4481e2ffe38324590dbce2941120de7a5
DamjanIgnjatic <ignjatic.dev@gmail.com>
2026-02-11 20:21:39 +0100

    footer

--- wp-content/themes/stella-horeca/footer.php
+++ wp-content/themes/stella-horeca/footer.php
@@ -1,38 +1,36 @@
 		<!-- Footer -->
-		<footer class="footer light-content" role="contentinfo">
+		<footer class="section footer light-content" role="contentinfo">
 			<?php if (!is_page_template('templates/template-plain.php')): ?>
 				<?php if (function_exists('get_field') && function_exists('dynamic_sidebar')): ?>
 					<?php $footerSection = get_field('footer_section', 'option') ?: []; ?>
 					<?php if ($footerSection && is_array($footerSection)): ?>
 						<?php if (array_key_exists('columns', $footerSection) && $columns = $footerSection['columns']): ?>
 							<?php if (is_array($columns) && !empty($columns)): ?>
-								<section class="footer-body-section">
-									<div class="container">
-										<div class="row footer-body pt-4">
-											<?php foreach ($columns as $no => $column): ?>
-												<?php $no++; ?>
-												<div class="<?php echo $column['column']; ?> section-<?php echo $no; ?>">
-													<?php
-													register_sidebar([
-														'name' => 'Footer section ' . $no,
-														'description' => 'Footer section ' . $no,
-														'id' => "footer-section-{$no}",
-														'before_widget' => '<div id="%1$s" class="widget footer-widget %2$s">',
-														'after_widget' => '</div>',
-														'before_title' => '<h3 class="widget-title">',
-														'after_title' => '</h3>',
-													]);
-													?>
-													<?php dynamic_sidebar("footer-section-{$no}"); ?>
-												</div>
-											<?php endforeach; ?>
-										</div>
+								<div class="container">
+									<div class="row footer-body pt-4">
+										<?php foreach ($columns as $no => $column): ?>
+											<?php $no++; ?>
+											<div class="<?php echo $column['column']; ?> section-<?php echo $no; ?>">
+												<?php
+												register_sidebar([
+													'name' => 'Footer section ' . $no,
+													'description' => 'Footer section ' . $no,
+													'id' => "footer-section-{$no}",
+													'before_widget' => '<div id="%1$s" class="widget footer-widget %2$s">',
+													'after_widget' => '</div>',
+													'before_title' => '<h3 class="widget-title">',
+													'after_title' => '</h3>',
+												]);
+												?>
+												<?php dynamic_sidebar("footer-section-{$no}"); ?>
+											</div>
+										<?php endforeach; ?>
 									</div>
-								</section>
+								</div>
 							<?php endif; ?>
 						<?php endif; ?>
 						<?php if (array_key_exists('copyright_text', $footerSection) && $copyright = $footerSection['copyright_text']): ?>
-							<div class="<?php echo !is_page_template('templates/template-plain.php') ? 'copyright pt-4' : ''; ?> text-center">
+							<div class="<?php echo !is_page_template('templates/template-plain.php') ? 'copyright' : ''; ?> text-center">
 								<?php if ($copyright): ?>
 									<small>
 										<?php echo str_replace(['[y]', '[Y]'], date('Y'), $copyright); ?>
