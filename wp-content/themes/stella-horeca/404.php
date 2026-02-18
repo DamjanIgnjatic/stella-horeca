e3a5205fcadd42f6de518161748be3897462916a
DamjanIgnjatic <ignjatic.dev@gmail.com>
2026-02-18 20:19:53 +0100

    404 page

--- wp-content/themes/stella-horeca/404.php
+++ wp-content/themes/stella-horeca/404.php
@@ -1,11 +1,6 @@
 <?php get_header(); ?>
 <main role="main">
 	<?php
-	$pid = get_field('custom_404_page', 'option');
-	if (function_exists('pll_get_post')) {
-		$pid = pll_get_post($pid);
-	}
-	$page = get_page($pid);
 	?>
 	<?php wp_reset_postdata(); ?>
 	<?php if ($page && isset($page->ID)): ?>
@@ -13,14 +8,12 @@
 			<?php echo apply_filters('the_content', $page->post_content); ?>
 		</div>
 	<?php else: ?>
-		<div class="content text-center">
-			<h1 class="h1 h0">404</h1>
-			<p class="text">
-				<b><?php _e('Ouch!', 'stellahoreca'); ?></b><br>
-				<?php _e('It looks that the page will not arrive.', 'stellahoreca'); ?><br>
-				<?php _e('Don\'t wait more!', 'stellahoreca'); ?>
-			</p>
-			<a href="<?php echo home_url(); ?>" class="btn go-home"><?php _e('Go Home', 'stellahoreca'); ?></a>
+		<div class="section container">
+			<div class="error-page">
+				<h2>Tražena <strong>stranica</strong> nije pronađena<strong>.</strong></h2>
+
+				<a href="<?php echo home_url(); ?>" class="horeca-btn"><?php _e('Glavna Stranica', 'stellahoreca'); ?></a>
+			</div>
 		</div>
 	<?php endif; ?>
 	<?php wp_reset_postdata(); ?>
